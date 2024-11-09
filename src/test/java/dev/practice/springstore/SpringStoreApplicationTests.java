1f38e4d10c3ff7f333c501cd79a65883f9d420c7
Kelvin <kelvintawiah224@gmail.com>
2024-11-08 22:24:33 -0600

    setup spring project

---
+++ src/test/java/dev/practice/springstore/SpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.springstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
